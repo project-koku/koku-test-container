4f11104e729463cbaa1ae987a1dfbd0f1da955ab
Sam Doran <sdoran@redhat.com>
2024-10-15 20:39:34 -0400

    Add no-cache option to build

--- build.py
+++ build.py
@@ -8,13 +8,19 @@ def main() -> None:
     parser.add_argument('--container-runtime', default='docker')
     parser.add_argument('image', nargs='?', default='koku-test-container')
     parser.add_argument('--version', default='latest')
+    parser.add_argument('--no-cache', action='store_true')
     parser.add_argument('--file', '-f', default='Containerfile')
 
     args = parser.parse_args()
     container_runtime = args.container_runtime
     tag = f'{args.image}:{args.version}'
 
-    subprocess.run([container_runtime, 'build', '--tag', tag, '--file', args.file, '.'], check=True)
+    command = [container_runtime, 'build', '--tag', tag, '--file', args.file, '.']
+
+    if args.no_cache:
+        command.insert(2, '--no-cache')
+
+    subprocess.run(command, check=True)
 
 
 if __name__ == '__main__':
