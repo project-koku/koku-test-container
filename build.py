fa4ac88bde16013d0e587cad11bf15a9958d0036
Sam Doran <sdoran@redhat.com>
2024-10-30 01:07:09 -0400

    Update default tag

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ import subprocess
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--container-runtime", default="docker")
-    parser.add_argument("image", nargs="?", default="koku-test-container")
+    parser.add_argument("image", nargs="?", default="quay.io/project-koku/koku-test-container")
     parser.add_argument("--version", default="latest")
     parser.add_argument("--no-cache", action="store_true")
     parser.add_argument("--file", "-f", default="Containerfile")
