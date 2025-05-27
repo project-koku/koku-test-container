faeaafddc2fa009435f891c12a4ce142352ccdc9
Lucas Bacciotti <lbacciot@redhat.com>
2025-05-27 09:53:10 +0100

    Increase timeout for full-run smoke tests to 8 hours (#36)

--- files/bin/deploy.py
+++ files/bin/deploy.py
@@ -86,7 +86,7 @@ def get_timeout(env_var: str, labels: set[str] | None = None) -> int:
         timeout = 2 * 60 * 60
 
     if labels and "full-run-smoke-tests" in labels:
-        timeout = 6 * 60 * 60
+        timeout = 8 * 60 * 60
 
     return int(timeout)
 
