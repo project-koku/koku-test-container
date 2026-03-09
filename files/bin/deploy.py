512c545bd9d7eb9826398919713e35cde1159c4a
Lucas Bacciotti <lbacciot@redhat.com>
2026-03-09 17:17:47 +0000

    Update event type check in deploy scripts to use 'push' instead of 'pull_request'

--- files/bin/deploy.py
+++ files/bin/deploy.py
@@ -183,7 +183,7 @@ def main() -> None:
 
     owner, repo = snapshot.components[0].source.git.url.path.split("/")[1:]
     pr_number = os.environ.get("PR_NUMBER", "")
-    if os.environ.get("EVENT_TYPE", "") != "pull_request":
+    if os.environ.get("EVENT_TYPE", "") == "push":
         pr_number = ""
     labels = get_pr_labels(pr_number, owner=owner, repo=repo) if pr_number else []
     app_name = os.environ.get("APP_NAME")
