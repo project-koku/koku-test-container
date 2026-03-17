b80b5e2d1dc8b8806648832cf35345caed61dd0b
Daniel Chorvatovic <dchorvat@redhat.com>
2026-03-17 10:59:59 +0100

    add comment

--- files/bin/deploy-iqe-cji.py
+++ files/bin/deploy-iqe-cji.py
@@ -179,6 +179,8 @@ class IQERunner:
         if "force-csv-splitting" in self.pr_labels:
             iqe_marker_expression = f"{iqe_marker_expression} or cost_force_csv_splitting"
         if "on-prem-processing" in self.pr_labels:
+            # added to prevent adding gcp/azure sources without support
+            # remove the exclusion after we have full cloud support in onprem deployments
             iqe_marker_expression = f"{iqe_marker_expression} and not cost_exclude_ocp_smokes"
 
         return iqe_marker_expression
