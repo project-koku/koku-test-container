79f38edf6d0039e92641d919e0a7010605bd7d92
Eva Sebestova <esebesto@redhat.com>
2026-04-15 14:17:40 +0200

    Exclude cost_onprem_blocked tests from ONPREM pr-checks

--- files/bin/deploy-iqe-cji.py
+++ files/bin/deploy-iqe-cji.py
@@ -178,9 +178,11 @@ class IQERunner:
         # Toggle test data generation to force CSV file splitting
         if "force-csv-splitting" in self.pr_labels:
             iqe_marker_expression = f"{iqe_marker_expression} or cost_force_csv_splitting"
-        if "on-prem-processing" in self.pr_labels and "cost_exclude_ocp_smokes" not in iqe_marker_expression:
-            # added to prevent adding gcp/azure sources without full support
-            iqe_marker_expression += " and not cost_exclude_ocp_smokes"
+        if "on-prem-processing" in self.pr_labels:
+            iqe_marker_expression += " and not cost_onprem_blocked"
+            if "cost_exclude_ocp_smokes" not in iqe_marker_expression:
+                # added to prevent adding gcp/azure sources without full support
+                iqe_marker_expression += " and not cost_exclude_ocp_smokes"
 
         return iqe_marker_expression
 
